39d1b6c22ea2b190b41b4f2dd66a81fb6d9fcad6
Christian Perich <christianperich@gmail.com>
2026-07-14 17:38:58 -0400

    Primer commit

---
+++ MatchData.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name MatchData
+
+var team1 : TeamData
+var team2 : TeamData
+var goals1 : int
+var goals2 : int
+var player : bool = false
---
+++ TeamData.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name TeamData
+
+@export var name : String = ""
+@export var strength : int
